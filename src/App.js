555af0a0b37201ff18183ae278a9133045b54672
esamrezk4 <92589088+esamrezk4@users.noreply.github.com>
2023-07-12 23:07:33 +0300

    handle dispatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./index.css";
 import Header from "./Header";
 import Main from "./Main";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
+const initialstate = {
+  questions: [],
+
+  // "loading" , "error" , "ready" , "active" , "finished"
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      break;
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialstate);
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
