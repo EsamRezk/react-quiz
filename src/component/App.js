9dae965d0a92ae4bb5d32107ba622af275ae7f4f
esamrezk4 <92589088+esamrezk4@users.noreply.github.com>
2023-07-15 23:05:13 +0300

    restart quiz

--- src/component/App.js
+++ src/component/App.js
@@ -15,6 +15,7 @@ const initialstate = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -41,16 +42,22 @@ function reducer(state, action) {
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
     case "finish":
-      return { ...state, status: "finished" };
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return { ...initialstate, questions: state.questions, status: "ready" };
+
     default:
       break;
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialstate
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialstate);
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
     0
@@ -101,6 +108,8 @@ export default function App() {
           <FinishScreen
             points={points}
             maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
--- src/component/FinishScreen.jsx
+++ src/component/FinishScreen.jsx
@@ -1,10 +1,30 @@
-function FinishScreen({ maxPossiblePoints, points }) {
+import { Fragment } from "react";
+
+function FinishScreen({ maxPossiblePoints, points, highscore, dispatch }) {
   const pers = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+
+  if (pers === 100) emoji = "🥇";
+  if (pers >= 80 && pers < 100) emoji = "🔥";
+  if (pers >= 50 && pers < 80) emoji = "😁";
+  if (pers >= 0 && pers < 50) emoji = "😔";
+  if (pers === 0) emoji = "😭";
   return (
-    <p className="result">
-      you finished this quiz with score <strong>{points}</strong> out of{" "}
-      {maxPossiblePoints} ({Math.ceil(pers)}%)
-    </p>
+    <Fragment>
+      <p className="result">
+        <span>{emoji}</span> you finished this quiz with score{" "}
+        <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(pers)}
+        %)
+      </p>
+      <p className="highscore">(highScore : {highscore} points)</p>
+      <button
+        className="btn btn-ui "
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
+    </Fragment>
   );
 }
 
